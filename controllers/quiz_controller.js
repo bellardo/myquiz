6cb703d31875913cf869d51e8be3eb8675523ea8
Albert Clavé <aclave@terlas.com>
2015-07-16 08:51:44 +0200

    Autoload

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,5 +1,34 @@
 var models = require('../models/models.js');
 
+// Autoload - factoriza el código si ruta incluye :quizId
+exports.load = function(req, res, next, quizId) {
+	models.Quiz.find(quizId).then(
+		function(quiz) {
+			if (quiz) {
+				req.quiz = quiz;
+				next();	
+
+			} else { 
+				next(new Error('No existe quizId=' +  quizId));
+
+			}
+
+		}
+
+	).catch(function(error) { next(error); });
+
+};
+
+
+// GET /quizes
+exports.index = function(req, res) {
+	models.Quiz.findAll().then(function(quizes) {
+		res.render('quizes/index', {quizes: quizes});
+
+	});
+
+};
+
 // GET /quizes/:id
 exports.show = function(req, res) {
 	models.Quiz.find(req.params.quizId).then(function(quiz) {
@@ -22,15 +51,6 @@ exports.answer = function(req, res) {
 
 	});
 	
-};
-
-// GET /quizes
-exports.index = function(req, res) {
-	models.Quiz.findAll().then(function(quizes) {
-		res.render('quizes/index', {quizes: quizes});
-
-	});
-
 };
 
 exports.author = function(req, res) {
