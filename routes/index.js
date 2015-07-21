c4ced6ce59a005a95d9bbb97f5bd5038a64601fa
Albert Clavé <aclave@terlas.com>
2015-07-21 17:29:15 +0200

    12 Validación de entradas

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var quizController = require('../controllers/quiz_controller');
 
 // Página de entrada (home page)
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz' });
+  res.render('index', { title: 'Quiz', errors: [] });
   
 });
 
@@ -17,7 +17,7 @@ router.get('/quizes', quizController.index);	// El parametro search és opcional
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 router.get('/quizes/new', quizController.new);
-router.post('/quizes/create', quizController.create)
+router.post('/quizes/create', quizController.create);
 
 router.get('/author', quizController.author);
 
