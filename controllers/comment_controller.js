076099127458e5f13eda716062207e38ea923c55
Albert Clavé <albertclave@gmail.com>
2015-08-06 21:19:53 +0200

    OL 8

---
+++ controllers/comment_controller.js
@@ -0,0 +1,43 @@
+var models = require('../models/models.js');
+
+
+// GET /quizes/:quizId/comments/new
+exports.new = function(req, res) {
+	res.render('comments/new', {quizid: req.params.quizId, errors: []});
+
+};
+
+// POST /quizes/:quizId/comments/create
+exports.create = function(req, res) {
+	var comment = models.Comment.build(
+			{texto: req.body.comment.texto, QuizId: req.params.quizId}
+		);
+
+	var hay_error = comment.validate();
+
+	if (hay_error) {
+		// Los errores són devueltos con el formato:
+ 		// { pregunta: [ '-> Falta Pregunta' ], respuesta: [ '-> Falta Respuesta' ] }
+
+		// Recuperamos los errores
+		var errs = [];
+		for (var i in hay_error) {
+			errs = errs.concat(hay_error[i]);
+
+		}
+
+		res.render('comments/new', {comment: comment, quizid: req.params.quizId, errors: errs});
+
+	} else {
+		// guarda en DB los campos pregunta y respuesta de quiz
+		comment.save().then(
+			function() {
+				res.redirect('/quizes/' + req.params.quizId);
+
+			}
+
+		); 
+
+	}
+
+};
